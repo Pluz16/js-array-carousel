c4be24b8a12055660110d1c71d5f06403a31996a
Francesco Punta <puntafrancesco@gmail.com>
2023-02-07 10:03:39 +0100

    Correzione classe

--- js/app.js
+++ js/app.js
@@ -9,13 +9,41 @@ console.log(ElementiSlides)
 const freccia_sx= document.getElementById('freccia_sx')
 const freccia_dx= document.getElementById('freccia_dx')
 
+let currentIndex = 0
+
 
 // Aggancio ad eventlistener
 freccia_sx.addEventListener('click', function(){
 	console.log('prev slide')
+
+	if (currentIndex > 0){
+	// nascondo attiva
+	ElementiSlides[currentIndex].classList.remove('active')
+
+	// mostro precedente
+	console.log(ElementiSlides[currentIndex - 1])
+	ElementiSlides[currentIndex - 1].classList.add('active')
+
+	currentIndex--
+	}
 })
 
 freccia_dx.addEventListener('click', function(){
 	console.log('next slide')
+
+	const lastIndex = ElementiSlides.lenght - 1
+
+	if(currentIndex < lastIndex) {
+
+	// Nascondo attiva
+	ElementiSlides[currentIndex].classList.remove('active')
+
+	// mostro successiva
+	ElementiSlides[currentIndex + 1].classList.add('active')
+
+	// aggiorno currentIndex
+	currentIndex += 1
+	}
 })
+
 
