4172ebe0b7e0d065ba4378fb8a8272c8570b8923
Francesco Punta <puntafrancesco@gmail.com>
2023-02-07 10:45:01 +0100

    Classe

--- js/app.js
+++ js/app.js
@@ -1,9 +1,46 @@
 console.log('carousel')
 
-let slides = document.querySelectorAll('.carosello .slide')
-ElementiSlides = [...slides]
 
+let slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+]
+
+// Recuperiamo dal dom il carosello
+const carosello = document.querySelector('.carosello')
+console.log(carosello)
+
+for(let i = 0; i < images.lenght; i++){
+	const srcImage = images[i]
+	console.log(srcImage)
+
+	if (i == 0) {
+		const htmlSlide =
+		<div class="slide active">
+			<img src ="${srcImage}" alt="">
+		</div>
+
+	} else{
+		htmlSlide =
+		<div class="slide">
+			<img src ="${srcImage}" alt="">
+		</div>
+	}
+
+	let className = 'slide'
+
+
+
+	carosello.innerHTML +- html
+}
+
+let slides = document.querySelectorAll('.carosello .slide')
+// ElementiSlides = [...slides]
 console.log(ElementiSlides)
+
 
 // Recupero controlli
 const freccia_sx= document.getElementById('freccia_sx')
